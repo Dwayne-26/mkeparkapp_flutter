5f640a15772bca3085abf6f7d364fc58fe489db6
Dwayne <d.sampson@MacBookPro.lan>
2025-11-25 21:53:52 -0600

    Disambiguate log usage in risk alert service

--- lib/services/risk_alert_service.dart
+++ lib/services/risk_alert_service.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:developer';
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:geolocator/geolocator.dart';
 
@@ -126,14 +126,14 @@ class RiskAlertService {
     const R = 6371000; // meters
     final dLat = _deg2rad(lat2 - lat1);
     final dLon = _deg2rad(lon2 - lon1);
-    final a = sin(dLat / 2) * sin(dLat / 2) +
-        cos(_deg2rad(lat1)) *
-            cos(_deg2rad(lat2)) *
-            sin(dLon / 2) *
-            sin(dLon / 2);
-    final c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    final a = math.sin(dLat / 2) * math.sin(dLat / 2) +
+        math.cos(_deg2rad(lat1)) *
+            math.cos(_deg2rad(lat2)) *
+            math.sin(dLon / 2) *
+            math.sin(dLon / 2);
+    final c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a));
     return R * c;
   }
 
-  double _deg2rad(double deg) => deg * (pi / 180);
+  double _deg2rad(double deg) => deg * (math.pi / 180);
 }
